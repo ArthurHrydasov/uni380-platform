4228b88a1dbffa34352b313adaa79eb0ad8ac1e6
ArthurHrydasov <69531159+ArthurHrydasov@users.noreply.github.com>
2025-07-03 18:46:17 +0300

    Update first-lesson.js

--- first-lesson.js
+++ first-lesson.js
@@ -84,7 +84,7 @@ function create_lesson(collection_id, user_email, user_id) {
     user_id: user_id
   };
   console.log(data);
-  fetch("https://uni-platform-6db9f53f05ed.herokuapp.com/create-lesson", {
+  fetch("https://uni380-ua.onrender.com/create-lesson", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -1215,7 +1215,7 @@ buttonWithAttrs.forEach((buttonWithAttr) => {
       };
       // console.log(data);
 
-      fetch("https://uni-platform-6db9f53f05ed.herokuapp.com/lesson-info", {
+      fetch("https://uni380-ua.onrender.com/lesson-info", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -1645,7 +1645,7 @@ theoryButtons.forEach((theoryButton) => {
 
       // console.log(data2);
 
-      fetch("https://uni-platform-6db9f53f05ed.herokuapp.com/lesson-info", {
+      fetch("https://uni380-ua.onrender.com/lesson-info", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
