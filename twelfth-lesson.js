3610532f81cd9f7bba91fcbff9dc1f7c7fbce569
ArthurHrydasov <69531159+ArthurHrydasov@users.noreply.github.com>
2025-07-03 18:59:54 +0300

    Update twelfth-lesson.js

--- twelfth-lesson.js
+++ twelfth-lesson.js
@@ -194,7 +194,7 @@ function create_lesson(collection_id, user_email, user_id) {
     user_id: user_id,
   };
   // console.log(data);
-  fetch("https://uni-platform-6db9f53f05ed.herokuapp.com/create-lesson", {
+  fetch("https://uni380-ua.onrender.com/create-lesson", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -1514,7 +1514,7 @@ buttonWithAttrs.forEach((buttonWithAttr) => {
       };
       console.log(data);
 
-      fetch("https://uni-platform-6db9f53f05ed.herokuapp.com/lesson-info", {
+      fetch("https://uni380-ua.onrender.com/lesson-info", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -2196,7 +2196,7 @@ theoryButtons.forEach((theoryButton) => {
         };
       }
       console.log(data2);
-      fetch("https://uni-platform-6db9f53f05ed.herokuapp.com/lesson-info", {
+      fetch("https://uni380-ua.onrender.com/lesson-info", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
